5b5854a400f4b7d88545e2a4579bf547ae7ba00d
Gracjan Bobowski <gracjanbobowski@gmail.com>
2023-12-10 13:36:36 +0100

    Inicjalizacja Projektu

---
+++ src/main/java/com/example/auctionservicesaplication/AuctionServicesAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.auctionservicesaplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionServicesAplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionServicesAplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auctionservicesaplication/AuctionServicesAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auctionservicesaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionServicesAplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
