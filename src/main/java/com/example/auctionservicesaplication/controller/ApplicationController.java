d32d366073a8ad89efd9ba3a6ee039c73068adf3
Gracjan Bobowski <gracjanbobowski@gmail.com>
2024-01-13 15:03:03 +0100

    Add_ApplicationController+Home.html

--- src/main/java/com/example/auctionservicesaplication/controller/ApplicationController.java
+++ src/main/java/com/example/auctionservicesaplication/controller/ApplicationController.java
@@ -1,7 +1,12 @@
 package com.example.auctionservicesaplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ApplicationController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
