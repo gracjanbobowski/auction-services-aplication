1cb58a0cd25e9116f83db20dde07cc800b0275fb
Gracjan Bobowski <gracjanbobowski@gmail.com>
2024-01-14 16:22:42 +0100

    SecurityConfig po Jarku ok

--- src/main/java/com/example/auctionservicesaplication/security/SecurityConfig.java
+++ src/main/java/com/example/auctionservicesaplication/security/SecurityConfig.java
@@ -1,41 +1,29 @@
-//package com.example.auctionservicesaplication.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/public/").permitAll() // Endpointy publiczne
-//                .antMatchers("/user/").hasRole("USER") // Endpointy dostępne tylko dla roli USER
-//                .antMatchers("/admin/**").hasRole("ADMIN") // Endpointy dostępne tylko dla roli ADMIN
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().permitAll()
-//                .and()
-//                .logout().permitAll();
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
-//                .and()
-//                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package com.example.auctionservicesaplication.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+//@EnableMethodSecurity(securedEnabled = true) // jak byście chcieli nad metodani pisać jakie są dostępy to trzeba to włączyć
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry
+                        -> authorizationManagerRequestMatcherRegistry
+                        .requestMatchers("/").hasAnyAuthority("USER", "ADMIN")
+                        .requestMatchers("/new").hasAnyAuthority("ADMIN")
+                        .requestMatchers("/edit/**").hasAnyAuthority("ADMIN")
+                        .requestMatchers("/delete/**").hasAuthority("ADMIN")
+                        .anyRequest().permitAll());
+        return httpSecurity.build();
+    }
+}
