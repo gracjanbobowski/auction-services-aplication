66ced1d77c3bfd8a61651e50dcf16d28cfa52873
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-01-14 15:59:00 +0100

    Zrobione Security

---
+++ src/main/java/com/example/auctionservicesaplication/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.auctionservicesaplication.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+//@EnableMethodSecurity(securedEnabled = true) // jak byście chcieli nad metodani pisać jakie są dostępy to trzeba to włączyć
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry
+                        -> authorizationManagerRequestMatcherRegistry
+                        .requestMatchers("/").hasAnyAuthority("USER", "ADMIN")
+                        .requestMatchers("/new").hasAnyAuthority("ADMIN")
+                        .requestMatchers("/edit/**").hasAnyAuthority("ADMIN")
+                        .requestMatchers("/delete/**").hasAuthority("ADMIN")
+                        .anyRequest().permitAll());
+        return httpSecurity.build();
+    }
+}
